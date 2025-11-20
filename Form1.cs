549a33723c69abf495e84d82b4e4cf143c28c1f0
Mozhde <mozhdehmozhdeh86@gmail.com>
2025-11-20 22:59:36 +0330

    Add files via upload

---
+++ Form1.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApplication1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            int[,] table = new int[10, 10];
+            for(int i=0;i<10;i++)
+            {
+                for(int j=0;j<10;j++)
+                {
+                    table[i, j] = (i + 1) * (j + 1);
+                }
+            }
+            for(int col=0;col<10;col++)
+            {
+                dataGridView1.Columns.Add("col" + col, (col + 1).ToString());
+            }
+            for (int row = 0; row < 10; row++)
+            {
+                object[] rowdata = new object[10];
+                for(int col=0;col<10;col++)
+                {
+                    rowdata[col] = table[row, col];
+                }
+                dataGridView1.Rows.Add(rowdata);
+            }
+            dataGridView1.AutoSizeColumnsMode = DataGridViewAutoSizeColumnsMode.AllCells;
+         }
+    }
+}
